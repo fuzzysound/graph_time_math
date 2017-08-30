1fa688d295bbf74f4b7b11ed2af3dada2d995a86
Junhyun Koh <hyun1652@gmail.com>
2017-08-30 16:16:24 +0900

    Initial commit

---
+++ _graph.py
@@ -0,0 +1,10 @@
+from graphviz import Digraph
+
+# TODO
+# 나열된 노드들의 계층 정보를 만족하는 그래프를 그리시오
+
+# TODO
+# 생성한 그래프를 6가지 layout engine을 사용해 다시 그리시오
+
+# TODO
+# 위 Case에는 어떤 layout이 가장 효과적으로 graph의 정보를 표현하고 있는지 서술하시오
---
+++ _math.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import math
+import statistics
+import random
+
+# TODO
+# Tkinter module을 이용해서 아래에 있는 function들을 지원하는 직관적인 GUI를 만들어본다.
+# statistics module: mean(), median(), mode(), pstdev(), pvariance(), stdev(), variance()
+# math module: ceil(), floor(), fabs(), factorial(), fmod(), log(), pow(), sqrt(), sin(), cos(), tan(),
+# degrees(), radians(), cosh(), sinh(), tanh()
+# random module: random(), seed(), uniform(), randint(), randrange(), choice(), shuffle(), sample()
+
