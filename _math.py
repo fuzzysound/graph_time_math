3a43866fc85f8d56b179a797b33fb07fb6f14ac4
Junhyun Koh <hyun1652@gmail.com>
2017-09-05 22:01:22 +0900

    _math.py 주석까지 완성

--- _math.py
+++ _math.py
@@ -3,125 +3,157 @@ from tkinter import ttk
 from math import *
 from statistics import *
 from random import *
+from functionHelps import helps # 함수 도움말을 가져오기 위해 import
 
-root=Tk()
-root.title("Project")
-
-combo_frame = ttk.Frame(root)
-combo_frame.grid(column=0, row=0)
-
-input_frame = ttk.Frame(root)
-input_frame.grid(column=1, row=0)
-
-output_frame = ttk.Frame(root)
-output_frame.grid(column=1, row=1)
-
-button_frame = ttk.Frame(root)
-button_frame.grid(column=1, row=2)
-
-input_value_1 = StringVar()
-input_value_2 = StringVar()
-
+# 입력된 숫자가 정수면 소숫점 아래를 제거하고, 그렇지 않으면 소숫점 셋째 자리까지 반올림하여 출력하는 함수
 def float_to_int(num):
     if num.is_integer():
         return int(num)
     else:
         return round(num, 3)
 
+# 입력으로 int type 변수가 들어올 경우 그대로 출력,입력으로 float type 변수가 들어올 경우 float_to_int 함수 적용해 출력,
+# 입력으로 iterable한 객체가 들어올 경우 float_to_num 함수 map하여 출력하는 함수
 def int_check(num):
+    if isinstance(num, int):
+        return num
     if isinstance(num, float):
         return float_to_int(num)
     if isinstance(num, list):
         return list(map(float_to_int, num))
 
+# Set Seed 버튼에 bind하기 위한 함수
 def _seed():
-    if input_value_1.get():
-        seed(float(input_value_1.get()))
-    else:
+    if input_value_1.get(): # Entry에 특정 값이 입력되었을 경우
+        seed(float(input_value_1.get())) # 해당 값으로 seed initialize
+    else: # Entry가 빈칸일 경우
         seed()
 
-def comboselect(input_value_1, input_value_2):
+# Combobox에 bind하기 위한 함수
+def comboselect(input_value_1, input_value_2): # input_value_1과 input_value_2는 각각 entry
+
+    # Combobox에서 선택을 바꿀 때마다 input_frame의 모든 요소들을 지운다
     for widget in input_frame.winfo_children():
         widget.destroy()
-    if combo.get() == 'random':
-        seed_entry = Entry(input_frame, width=20, textvariable=input_value_1)
+
+    # 선택된 함수에 따라 entry 생성
+    if combo.get() == 'random': # 예외1: 선택된 함수가 random일 때
+        seed_entry = Entry(input_frame, width=20, textvariable=input_value_1) # Seed가 입력될 entry
         seed_entry.grid(column=1, row=1)
-        Label(input_frame, text='(').grid(column=0, row=1, sticky=E)
-        Label(input_frame, text=')').grid(column=2, row=1)
-        seed_button = Button(input_frame, text = 'Set Seed', command = _seed)
+        Label(input_frame, text='(').grid(column=0, row=1, sticky=E) # Entry 좌로 여는 괄호
+        Label(input_frame, text=')').grid(column=2, row=1) # Entry 우로 닫는 괄호
+        seed_button = Button(input_frame, text = 'Set Seed', command = _seed) # Set Seed 버튼 생성, _seed 함수와 bind
         seed_button.grid(column=3, row=1)
-    elif combo.get() == 'sample':
-        list_entry = Entry(input_frame, width=20, textvariable=input_value_1)
-        value_entry = Entry(input_frame, width=20, textvariable=input_value_2)
+    elif combo.get() == 'sample': # 예외2: 선택된 함수가 sample일 때
+        list_entry = Entry(input_frame, width=20, textvariable=input_value_1) # Population이 입력될 entry
+        value_entry = Entry(input_frame, width=20, textvariable=input_value_2) # sample 수 값이 입력될 entry
         list_entry.grid(column=1, row=1)
-        Label(input_frame, text='(').grid(column=0, row=1, sticky=E)
-        Label(input_frame, text=',').grid(column=2, row=1)
         value_entry.grid(column=1, row=2)
-        Label(input_frame, text=')').grid(column=2, row=2)
-    else:
-        entry = Entry(input_frame, width=20, textvariable=input_value_1)
+        Label(input_frame, text='(').grid(column=0, row=1, sticky=E) # 첫번째 entry 좌로 여는 괄호
+        Label(input_frame, text=',').grid(column=2, row=1) # 두 entry 사이 콤마
+        Label(input_frame, text=')').grid(column=2, row=2) # 두번째 entry 우로 닫는 괄호
+    else: # 그 외 모든 함수에 대해서 일괄 적용
+        entry = Entry(input_frame, width=20, textvariable=input_value_1) # 입력값이 입력될 entry
         entry.grid(column=1, row=1)
-        Label(input_frame, text='(').grid(column=0, row=1, sticky=E)
-        Label(input_frame, text=')').grid(column=2, row=1)
-    Label(output_frame, text='=').grid(column=0, row=0)
+        Label(input_frame, text='(').grid(column=0, row=1, sticky=E) # entry 좌로 여는 괄호
+        Label(input_frame, text=')').grid(column=2, row=1) # entry 우로 닫는 괄호
 
-    button['command'] = calculate
+    # helper canvas 삭제 및 새로 생성
+    helper_canvas.delete('all') # 기존에 helper canvas가 존재할 경우 지운다
+    margin = 10 # 테두리 여백
+    helper_canvas.create_rectangle(margin, margin, width - margin, height - margin, outline='dimgray') # 테두리 상자 생성
+    root_bg = root.cget('bg') # root 윈도우의 배경색 추출
+    helper_canvas.create_rectangle(margin * 5 - 15, margin - 15, margin * 5 + 15, margin + 15, fill=root_bg,
+                                   outline=root_bg) # Help 글자가 들어갈 만큼의 공간을 사각형으로 만든다. 일종의 꼼수.
+    helper_canvas.create_text(margin * 5, margin, text='Help') # Help 글자 생성
+    helper_canvas.create_text(width / 2, height / 2, text=helps[str(combo.get())],
+                              width=width - 4 * margin) # 해당 함수에 해당하는 도움말을 helps에서 가져와서 띄운다
 
+    # 그 외
+    Label(output_frame, text='=').grid(column=0, row=0) # 출력값 왼쪽에 '=' 기호 띄우기
+    button['command'] = calculate # Calculate 버튼에 calculate 함수 bind
+
+#  Calculate 버튼에 bind하기 위한 함수
 def calculate():
     try:
-        if combo.get() == 'random':
-            cal_result = random()
-            cal_result = int_check(cal_result)
-            output_value.set(cal_result)
-        elif combo.get() == 'shuffle':
-            value = list(map(float, input_value_1.get().split(',')))
-            shuffle(value)
-            value = int_check(value)
-            output_value.set(value)
-        elif combo.get() == 'sample':
-            population = list(map(float, input_value_1.get().split(',')))
-            num_sample = int(input_value_2.get())
-            cal_result = sample(population, num_sample)
-            cal_result = int_check(cal_result)
-            output_value.set(cal_result)
-        else:
+        if combo.get() == 'random': # 예외1: 선택된 함수가 random일 때
+            cal_result = random() # random 함수로 값 생성
+            cal_result = int_check(cal_result) # int_check 함수 적용
+            output_value.set(cal_result) # 출력
+        elif combo.get() == 'shuffle': # 예외2: 선택된 함수가 shuffle일 때
+            value = list(map(float, input_value_1.get().split(','))) # 입력값을 받아 콤마 단위로 분리한 다음 float화하여 list로 변환
+            shuffle(value) # shuffle 함수는 아무것도 return하지 않기 때문에, 직접 return해 주어야 한다
+            value = int_check(value) # int_check 함수 적용
+            output_value.set(value) # 출력
+        elif combo.get() == 'sample': # 예외3: 선택된 함수가 sample일 때
+            population = list(map(float, input_value_1.get().split(','))) # Population. 입력값을 받아 콤마 단위로 분리한 다음 float화하여 list로 변환
+            num_sample = int(input_value_2.get()) # Num. of samples. 입력값을 int화
+            cal_result = sample(population, num_sample) # sample 함수 적용해 결과 추출
+            cal_result = int_check(cal_result) # int_check 함수 적용
+            output_value.set(cal_result) # 출력
+        else: # 그 외 모든 함수에 대해서 일괄 적용
             try:
-                value = list(map(float, input_value_1.get().split(',')))
-                cal_result = eval(combo.get() + '(' + str(value) + ')')
-                cal_result = int_check(cal_result)
-                output_value.set(cal_result)
-            except:
+                value = list(map(float, input_value_1.get().split(','))) # 입력값을 받아 콤마 단위로 분리한 다음 float화하여 list로 변환
+                cal_result = eval(combo.get() + '(' + str(value) + ')') # 입력값을 인수로 삼아 해당 함수 바로 실행해 결과 추출
+                cal_result = int_check(cal_result) # int_check 함수 적용
+                output_value.set(cal_result) # 출력
+            except: # 인수로 iterable이 아닌 여러 변수를 받는 함수를 위한 예외문
                 try:
-                    value = list(map(float , input_value_1.get().split(',')))
-                    cal_result = eval(combo.get() + '(*' + str(value) + ')')
-                    cal_result = int_check(cal_result)
-                    output_value.set(cal_result)
+                    value = list(map(float , input_value_1.get().split(','))) # 입력값을 받아 콤마 단위로 분리한 다음 float화하여 list로 변환
+                    cal_result = eval(combo.get() + '(*' + str(value) + ')') # 입력값 앞에 asterisk를 붙여 unpack. 그 외 위와 동일
+                    cal_result = int_check(cal_result) # int_check 함수 적용
+                    output_value.set(cal_result) # 출력
                 except:
-                    pass
+                    pass # 입력값이 적절하지 않을 경우 pass
     except:
-        pass
+        pass # 입력값이 적절하지 않을 경우 pass
 
-def _comboselect(event):
-    comboselect(input_value_1, input_value_2)
+def _comboselect(event): # tkinter에서 widget에 함수를 bind할 경우 함수에 인수를 설정할 수 없으므로, 이를 우회하기 위한 꼼수
+    comboselect(input_value_1, input_value_2) # input_value_1과 input_value_2를 인수로 받아 comboselect 함수 실행
 
-combo = ttk.Combobox(combo_frame)
-combo.bind('<<ComboboxSelected>>', _comboselect)
+# Root window와 내부 frame 설정
+root=Tk() # root window 설정
+root.title("Advanced Calculator") # 제목표시줄에 들어갈 제목
 
+title_frame = ttk.Frame(root) # 가장 위에 표시될 프로그램 이름을 위한 frame 설정
+title = Label(title_frame, text='Advanced Calculator', font=('Comic Sans MS', 25, 'bold'), fg='darkcyan') # Label 설정
+title.pack()
+title_frame.grid(column=0, row=0, columnspan=2, padx=30, pady=20)
 
-
-
-combo.set('Select a function...')
+combo_frame = ttk.Frame(root) # Combobox가 들어갈 frame 설정
+combo = ttk.Combobox(combo_frame) # Combobox 생성
+combo.bind('<<ComboboxSelected>>', _comboselect) # _comboselect 함수와 bind
+combo.set('Select a function...') # 가장 처음에 combobox에 표시될 텍스트 설정
 combo['values'] = ('mean', 'median', 'mode', 'pstdev', 'pvariance', 'stdev', 'variance', 'ceil', 'floor', 'fabs',
                    'factorial', 'fmod', 'log', 'pow', 'sqrt', 'sin', 'cos', 'tan', 'degrees', 'radians', 'cosh',
-                   'sinh', 'tanh', 'random', 'seed', 'uniform', 'randint', 'randrange', 'choice', 'shuffle', 'sample')
+                   'sinh', 'tanh', 'random', 'uniform', 'randint', 'randrange', 'choice', 'shuffle', 'sample') # 선택지 설정
 combo.pack()
+combo_frame.grid(column=0, row=1, padx=5, pady=15, sticky=E)
 
+input_frame = ttk.Frame(root) # 입력값 entry가 들어갈 frame 설정. 구체적인 entry 설정은 comboselect 함수에서 일어남.
+input_frame.grid(column=1, row=1)
 
-output_value = StringVar()
-result = Label(output_frame, textvariable=output_value, font='Helvetica 20 bold')
+output_frame = ttk.Frame(root)  # 출력값 label이 들어갈 frame 설정.
+output_value = StringVar() # 출력값이 할당될 전역변수 설정
+result = Label(output_frame, textvariable=output_value, font='Helvetica 20 bold') # Label 생성
 result.grid(column=1, row=0, padx=15)
+output_frame.grid(column=1, row=2)
 
-button = ttk.Button(button_frame, text='Calculate')
+button_frame = ttk.Frame(root) # Calculate 버튼이 들어갈 frame 생성
+button = ttk.Button(button_frame, text='Calculate') # Calculate 버튼 생성
 button.pack()
+button_frame.grid(column=1, row=3, pady=10)
 
+helper_frame = ttk.Frame(root) # 함수 도움말이 들어갈 frame 설정.
+width = 200
+height = 200
+helper_canvas = Canvas(helper_frame, width=width, height=height) # Canvas 생성. 캔버스 내부 설정은 comboselect 함수에서 일어남.
+helper_canvas.pack()
+helper_frame.grid(column=0, row=2, rowspan=3)
+
+# 입력값이 할당될 전역변수 설정
+input_value_1 = StringVar()
+input_value_2 = StringVar()
+
+# 프로그램 실행
 root.mainloop()
---
+++ functionHelps.py
@@ -0,0 +1,36 @@
+# _math.py에 필요한 helper text
+
+helps = {
+    'mean': "mean(data): returns the sample arithmetic mean of the data.",
+    'median': "median(data): returns the median of the data.",
+    'mode': "mode(data): returns the most common data point from discrete or nominal data. If no unique value is mode, returns nothing.",
+    'pstdev': "pstdev(data): returns the population standard deviation.",
+    'pvariance': "pvariance(data): returns the population v ariance of data, a non-empty iterable of real-valued numbers.",
+    'stdev': "stdev(data): returns the sample standard deviation.",
+    'variance': "variance(data): returns the sample variance of data, an iterable of at least two real-valued numbers.",
+    'ceil': "ceil(x): returns the smallest integer greater than or equal to x.",
+    'floor': "floor(x): returns the largest integer less than or equal to x.",
+    'fabs': "fabs(x): returns the absolute value of x.",
+    'factorial': "factorial(x): returns the x factorial.",
+    'fmod': "fmod(x,y): returns the remainder of x divided by y.",
+    'log': "log(a,b): returns the value of log(_b)a",
+    'pow': "pow(a,b): returns the a raised to the power of b.",
+    'sqrt': "sqrt(a): returns the square root of a.",
+    'sin': "sin(x): returns the sine of x radians.",
+    'cos': "cos(x): returns the cosine of x radians.",
+    'tan': "tan(x): returns the tangent of x radians.",
+    'degrees': "degrees(x): converts angle x from radians to degrees.",
+    'radians': "radians(x): converts angle x from degrees to radians.",
+    'cosh': "cosh(x): returns the hyperbolic cosine of x.",
+    'sinh': "sinh(x): returns the hyperbolic sine of x.",
+    'tanh': "tanh(x): returns the hyperbolic tangent of x.",
+    'random': "random(seed): returns the next random floating point number in the range [0.0, 1.0). Initialize the seed with 'Set Seed' "
+            "button. If not initialized, seed will be set according to current system time.",
+    'uniform': "uniform(a,b): returns a random floating point number in [a,b].",
+    'randint': "randint(a,b): returns a random integer N in [a,b].",
+    'randrange': "randrange(a,b,c): returns a random integer N in [a,b) where N = a + kc (k is constant). If b and c are not given,"
+               "returns a random integer N in [0,a).",
+    'choice': "choice(data): returns a random element from data.",
+    'shuffle': "shuffle(data): returns a shuffled sequence of the data.",
+    'sample': "sample(data,a): returns a-length list of randomly chosen elements from the data."
+}
